d57078e9289a8270e1d77d5c441fa49b4cee08b2
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2021-06-04 12:24:33 +0300

    Update Todo.js

--- routes/Todo.js
+++ routes/Todo.js
@@ -17,8 +17,11 @@ router.get("/todos/",getTodos);
 router.get("/todo/:todoId/",getTodo);
 router.post("/todo/add/",addTodo);
 router.put("/todo/:todoId/update",updateTodo);
+//The only param you pass to the endpoind is todoId so that you can just create get route for this operation and return updated todo item for example)
+//router.get("/todo/:todoId/done",markDone);
 router.put("/todo/:todoId/done",markDone);
 router.put("/todo/:todoId/undone",markUndone);
+// Since it is delete route, you can just use router.delete("/todo/:todoId",removeTodo);
 router.delete("/todo/:todoId/remove",removeTodo);
 
 module.exports = router;
